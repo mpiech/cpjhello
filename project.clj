3bc2efd4e87368f44ed8434be45dbbcfdc14f9a3
MICHAEL PIECH <mpiech@TaklaMaka.local>
2017-03-28 19:20:42 -0700

    Added nREPL setup

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.clojure/tools.nrepl "0.2.12"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler cpjhello.handler/app :port 8080}
   :profiles
