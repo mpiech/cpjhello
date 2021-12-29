0c11eba37ee3656a1c849eb5503e5c523bf584d4
Mike Piech <mpiech@alum.mit.edu>
2021-12-29 13:16:46 -0800

    Updated packge versions

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject cpjhello "0.1.0-SNAPSHOT"
+(defproject cpjhello "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.13"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler cpjhello.handler/app :port 8080}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.2"]]}})
